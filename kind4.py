2777166f4b201b02a274f4c73876ec74535fa4ce
Brian Hartford <bhartford419@gmail.com>
2024-06-16 21:17:15 -0400

    Working kind 0 scan and update (#4)

--- kind4.py
+++ kind4.py
@@ -8,11 +8,29 @@ from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
-class Kind4MessageEncoder:
+class Kind4MessageCodec:
+    """
+    A codec for encrypting and decrypting messages using ECDH (Elliptic Curve Diffie-Hellman)
+    for key exchange and AES (Advanced Encryption Standard) in CBC mode for encryption.
+
+    Attributes:
+        hex_private_key (str): Hexadecimal string of the private key used for ECDH key exchange.
+        hex_public_key (str): Hexadecimal string of the public key used for ECDH key exchange.
+
+    Methods:
+        encrypt_message(message: str) -> str:
+            Encrypts a plaintext message using AES-CBC encryption with a shared key derived from ECDH key exchange.
+            Returns the encrypted message in base64 format followed by the IV in base64 as a query string.
+
+        decrypt_message(encrypted_message_with_iv: str) -> str:
+            Decrypts an encrypted message which includes the initialization vector (IV).
+            Expects the encrypted message in base64 format with the IV appended as a query string.
+            Returns the original plaintext message.
+    """
+
     def __init__(self, hex_private_key, hex_public_key):
         self.hex_private_key = hex_private_key
         self.hex_public_key = hex_public_key
-        # self.message = message
         self.private_key = self._generate_private_key()
         self.public_key = self._generate_public_key()
 
@@ -39,5 +57,19 @@ class Kind4MessageEncoder:
         encrypted_message = encryptor.update(padded_data) + encryptor.finalize()
         encrypted_message_base64 = base64.b64encode(encrypted_message).decode("utf-8")
         iv_base64 = base64.b64encode(iv).decode("utf-8")
-        stringed = f"{encrypted_message_base64}?iv={iv_base64}"
-        return stringed
+        return f"{encrypted_message_base64}?iv={iv_base64}"
+
+    def decrypt_message(self, encrypted_message_with_iv):
+        encrypted_message_base64, iv_base64 = encrypted_message_with_iv.split("?iv=")
+        encrypted_message = base64.b64decode(encrypted_message_base64)
+        iv = base64.b64decode(iv_base64)
+        shared_key = self.private_key.exchange(ec.ECDH(), self.public_key)
+        shared_x = shared_key[:32]
+        cipher = Cipher(
+            algorithms.AES(shared_x), modes.CBC(iv), backend=default_backend()
+        )
+        decryptor = cipher.decryptor()
+        padded_plaintext = decryptor.update(encrypted_message) + decryptor.finalize()
+        unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+        plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
+        return plaintext.decode("utf-8")
--- update_meta.py
+++ update_meta.py
@@ -1,17 +1,15 @@
+import argparse
 import asyncio
+import concurrent.futures
 import hashlib
 import json
 import logging
-import secp256k1
 import time
-import websockets
-import requests
-import asyncio
-import uvloop
-import concurrent.futures
-import time
-from kind4 import Kind4MessageEncoder
 
+import requests
+import secp256k1
+import uvloop
+import websockets
 
 logging.basicConfig(
     filename="./nost_query.log",
@@ -21,11 +19,11 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
 class NoteUpdater:
-    def __init__(self) -> None:
+    def __init__(self, pubkey_to_query) -> None:
         self.events_found = []
         self.good_relays = []
+        self.updated_relays = []
         self.bad_relays = []
         self.scanner_pubkey_hex = (
             "5ce5b352f1ef76b1dffc5694dd5b34126137184cc9a7d78cba841c0635e17952"
@@ -33,9 +31,7 @@ class NoteUpdater:
         self.scanner_privkey_hex = (
             "2b1e4e1f26517dda57458596760bb3bd3bd3717083763166e12983a6421abc18"
         )
-        self.pubkey_to_query = (
-            "4503baa127bdfd0b054384dc5ba82cb0e2a8367cbdb0629179f00db1a34caacc"#)"b05ec62a98702e0c202759eac905550dd9e4a2af9a3df856159207925b4f1c50"
-        )
+        self.pubkey_to_query = pubkey_to_query
         self.timestamp_set = set()
         self.high_time = 1
         self.all_good_relays = {}
@@ -44,7 +40,6 @@ class NoteUpdater:
         self.latest_note = ""
         self.relays_kind4 = [
             "wss://relay.nostpy.lol",
-            "wss://nostpy.serverless-nostr.com",
             "wss://damus.io",
             "wss://nostr.fmt.wiz.biz/",
             "wss://nostr-pub.wellorder.net/",
@@ -87,27 +82,6 @@ class NoteUpdater:
         data_str = json.dumps(data, separators=(",", ":"), ensure_ascii=False)
         return hashlib.sha256(data_str.encode("UTF-8")).hexdigest()
 
-    def create_event(self, public_key, private_key_hex, enc_cont):
-        tags = [["p", self.pubkey_to_query]]
-        created_at = int(time.time())
-        kind_number = 4
-        content = enc_cont
-        event_id = self.calc_event_id(
-            public_key, created_at, kind_number, tags, content
-        )
-        signature_hex = self.sign_event_id(event_id, private_key_hex)
-        event_data = {
-            "id": event_id,
-            "pubkey": public_key,
-            "kind": kind_number,
-            "created_at": created_at,
-            "tags": tags,
-            "content": content,
-            "sig": signature_hex,
-        }
-
-        return event_data
-
     def verify_signature(self, event_id: str, pubkey: str, sig: str) -> bool:
         try:
             pub_key = secp256k1.PublicKey(bytes.fromhex("02" + pubkey), True)
@@ -124,36 +98,25 @@ class NoteUpdater:
             logger.error(f"Error verifying signature for event {event_id}: {e}")
             return False
 
-    async def send_event(self, public_key, private_key_hex, enc_content):
+    async def _send_event_to_relay(self, relay, event_data):
         try:
-            event_data = self.create_event(public_key, private_key_hex, enc_content)
-            signature_valid = self.verify_signature(
-                event_data.get("id"), public_key, event_data.get("sig")
-            )
-            if signature_valid:
-                for ws_relay in self.relays_kind4:
-                    try:
-                        async with websockets.connect(ws_relay) as ws:
-                            logger.info("WebSocket connection created.")
+            async with websockets.connect(relay) as ws:
+                logger.info("WebSocket connection created.")
 
-                            event_json = json.dumps(("EVENT", event_data))
-                            await ws.send(event_json)
-                            logger.info(f"Event sent to {ws_relay}: {event_json}")
+                event_json = json.dumps(("EVENT", event_data))
+                await ws.send(event_json)
+                logger.info(f"Event sent to {relay}: {event_json}")
 
-                            response = await asyncio.wait_for(ws.recv(), timeout=10)
-                            response_data = json.loads(response)
-                            logger.info(
-                                f"DM response from {ws_relay} is {response_data}"
-                            )
-                    except asyncio.TimeoutError:
-                        logger.error(f"Timeout waiting for response from {ws_relay}.")
-                    except websockets.WebSocketException as wse:
-                        logger.error(f"WebSocket error with {ws_relay}: {wse}")
-                    except Exception as exc:
-                        logger.error(f"Error with {ws_relay}: {exc}")
+                response = await asyncio.wait_for(ws.recv(), timeout=10)
+                response_data = json.loads(response)
+                print(f"Response data is {response_data}")
 
+        except asyncio.TimeoutError:
+            logger.error(f"Timeout waiting for response from {relay}.")
+        except websockets.WebSocketException as wse:
+            logger.error(f"WebSocket error with {relay}: {wse}")
         except Exception as exc:
-            logger.error(f"Error creating or processing event: {exc}")
+            logger.error(f"Error with {relay}: {exc}")
 
     async def query_relay(self, relay, kinds=None):
         try:
@@ -164,10 +127,7 @@ class NoteUpdater:
                     "since": 179340343,
                 }
 
-                if kinds == [4]:
-                    query_dict["tags"] = ["p", self.scanner_pubkey_hex]
-                else:
-                    query_dict["authors"] = [self.pubkey_to_query]
+                query_dict["authors"] = [self.pubkey_to_query]
 
                 query_ws = json.dumps(("REQ", "5326483051590112", query_dict))
 
@@ -179,28 +139,18 @@ class NoteUpdater:
                     if response[0] == "EVENT":
                         if response[2]["kind"] == 0:
                             self.relay_event_pair[relay] = response
-                            return
-                        else:
-                            self.return_event = response[2]["pubkey"]
-
+                            return response[2]
                 except asyncio.TimeoutError:
                     logger.info("No response within 1 second, continuing...")
         except Exception as exc:
             logger.error(f"Exception is {exc}, error querying {relay}")
-
-    async def query_kind4(self):
-        tasks = [
-            asyncio.create_task(self.query_relay(relay, kinds=[4]))
-            for relay in self.relays_kind4
-        ]
-        await asyncio.gather(*tasks)
 
     def integrity_check_whole(self):
         for relay in self.relay_event_pair:
             value = self.relay_event_pair[relay]
             note = value[2]
             if (
-                note != None
+                note is not None
                 and note["pubkey"] == self.pubkey_to_query
                 and note["kind"] == 0
             ):
@@ -234,15 +184,19 @@ class NoteUpdater:
             async with websockets.connect(relay) as ws:
                 event_json = json.dumps(("EVENT", self.latest_note))
                 await ws.send(event_json)
-                logger.info(
-                    f"rebroadcasting latest kind 0: {event_json} note to: {relay}"
+                print(
+                    f"Rebroadcasting latest kind 0: {event_json} note to:  \033[1;32m{relay}\033[0m"
                 )
                 response = json.loads(await asyncio.wait_for(ws.recv(), timeout=3))
-                logger.debug(f"response 1 is {response[1]}")
-                if response[2] == self.latest_note:
-                    print(f"Relay: {relay} now has latest event: {self.latest_note}")
+                logger.info(f"Event ID is {response[1]}")
+                if response[2] in ["true", "True"]:
+                    self.updated_relays.append(relay)
+        except asyncio.TimeoutError:
+            logger.error(f"Timeout waiting for response from {relay}.")
+        except websockets.WebSocketException as wse:
+            logger.error(f"WebSocket error with {relay}: {wse}")
         except Exception as exc:
-            logger.error(f"Error rebroadcasting to {relay} : {exc}")
+            logger.error(f"Error rebroadcasting to {relay}: {exc}")
 
     async def gather_rebroadcast(self):
         tasks = [
@@ -257,10 +211,10 @@ class NoteUpdater:
 
     async def list_old_relays(self, timestamp, relay):
         if timestamp < self.high_time:
-            print(f"Realy {relay} had old event")
+            print(f"Relay {relay} had old event")
 
     def calc_old_relays(self):
-        print(f"Newest timestmap is: {self.high_time}")
+        print(f"Newest timestamp is: {self.high_time}")
         for relay in self.all_good_relays:
             if self.all_good_relays[relay] < self.high_time:
                 message = (
@@ -272,22 +226,19 @@ class NoteUpdater:
             elif self.all_good_relays[relay] == self.high_time:
                 pass
 
-
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-
-async def main():
+async def main(pubkey_to_query):
     try:
         start_time = time.time()
 
-        update_obj = NoteUpdater()
+        update_obj = NoteUpdater(pubkey_to_query)
         update_obj.print_color("Starting to query relays", "34")
 
         loop = asyncio.get_event_loop()
-        executor = concurrent.futures.ThreadPoolExecutor(max_workers=200)
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=100)
         loop.set_default_executor(executor)
 
-        await update_obj.query_kind4()
         await update_obj.gather_queries()
 
         gather_time = time.time()
@@ -297,12 +248,12 @@ async def main():
 
         update_obj.integrity_check_whole()
         logger.info(
-            f"Relays that return the corect timestpamp are {update_obj.good_relays}, Relays with bad time stamps are: {update_obj.bad_relays}"
+            f"Relays that return the correct timestamp are {update_obj.good_relays}, Relays with bad time stamps are: {update_obj.bad_relays}"
         )
 
         update_obj.calc_old_relays()
         print(
-            f"Bad relays are : {update_obj.bad_relays}, old relays is {update_obj.old_relays}"
+            f"Relays with old timestamp are : {update_obj.bad_relays}, old relays are {update_obj.old_relays}"
         )
         calculate_time = time.time()
         update_obj.print_color(
@@ -319,26 +270,12 @@ async def main():
 
         final_time = time.time() - start_time
         update_obj.print_color(f"----- {final_time} seconds final time", "32")
-        try:
-            kind4_enc = Kind4MessageEncoder(
-                update_obj.scanner_privkey_hex, update_obj.pubkey_to_query
-            )
-            enc_content = kind4_enc.encrypt_message(
-                f"I corrected your relays on {update_obj.old_relays}"
-            )
-        except Exception as exc:
-            enc_content = "a"
-            print(f"enc cont error {exc}")
-        try:
-            await update_obj.send_event(
-                update_obj.scanner_pubkey_hex,
-                update_obj.scanner_privkey_hex,
-                enc_content,
-            )
-        except Exception as exc:
-            print(f"Exception is {exc}")
     except KeyboardInterrupt:
-        print(f"keybord int")
+        print(f"Keyboard interrupt")
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Query old kind 0 events.')
+    parser.add_argument('pubkey', type=str, help='Public key hex to query for old kind 0 events')
+    args = parser.parse_args()
 
-asyncio.run(main())
+    asyncio.run(main(args.pubkey))
